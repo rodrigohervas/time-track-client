052e072b9ef7764f5b70232460d7ce0e2075461d
Rodrigo Hervas <38118239+rodrigohervas@users.noreply.github.com>
2020-04-22 17:14:24 -0400

    Commented Enums

--- src/helpers/Enums.js
+++ src/helpers/Enums.js
@@ -1,4 +1,6 @@
+//enum listing all months of the year for Dashboard component
 export const Months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 
+//enum lsiting all Pto.types description for Pto component
 export const ptoTypes = ['Vacation days', 'Personal days', 'Sick days', 'Other']
 
