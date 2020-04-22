0b1bc81be65077e5ecc030cf790b4dca22cd657c
Rodrigo Hervas <38118239+rodrigohervas@users.noreply.github.com>
2020-04-22 16:42:32 -0400

    commented AuthWrapper

--- src/Authentication/AuthWrapper.js
+++ src/Authentication/AuthWrapper.js
@@ -1,12 +1,20 @@
 import React, {useEffect, useState} from 'react'
 import { useHistory } from 'react-router-dom'
 
+/**
+ * Authorization Component to validate if user is signed-in
+ * @param {object} props 
+ */
 function AuthWrapper(props) {
 
     const [username, setUsername] = useState(localStorage.getItem('username') || '')
     const [password, setPassword] = useState(localStorage.getItem('password') || '')
     const history = useHistory()
 
+    /**
+     * useEffect hook that validates is there user is signed-in, 
+     * then redirects to the children component (defined in App.js) or to SignIn component
+     */
     useEffect(() => {
         if (username === '' || password === '') {
             console.log('User not signed in, redirecting to signin')
